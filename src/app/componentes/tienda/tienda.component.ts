21f7b1289e48d3d91b268786478ca7ce82ccde65
Luis <ljmarquezg@gmail.com>
2019-08-25 14:49:44 +0200

    Tienda component updated

--- src/app/componentes/tienda/tienda.component.ts
+++ src/app/componentes/tienda/tienda.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { Carrito } from '../../modelos/Carrito';
+import { Carrito } from 'src/app/modelos/Carrito';
 import { Producto } from 'src/app/modelos/Producto';
 import { CartService } from 'src/app/services/cart/cart.service';
+import { TiendaService } from 'src/app/services/tienda/tienda.service';
 import { Item } from 'src/app/modelos/Items';
 
 @Component({
@@ -17,17 +18,32 @@ export class TiendaComponent implements OnInit {
   productosEnCarrito: Carrito;
   listaProductos: Item[] = [];
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private tiendaService: TiendaService) {
   }
 
   ngOnInit() {
-    this.productosEnCarrito = this.cartService.inicializarContador();
+    // //this.productosEnCarrito = this.cartService.inicializarContador();
+    // const getCart = this.tiendaService.obtenerCart();
+    // getCart.subscribe(cart => {
+    //   console.log(cart);
+    //   this.productosEnCarrito = cart;
+    // });
   }
 
+  // agregarACarrito(nuevoProducto: Producto): Carrito {
+  //   const observable = this.cartService.agregarAListaCarrito(nuevoProducto, 'sumar');
+  //   observable.subscribe(listado => {
+  //     this.productosEnCarrito = listado;
+  //   });
+  //   return this.productosEnCarrito;
+  // }
+
   agregarACarrito(nuevoProducto: Producto): Carrito {
-    const observable = this.cartService.agregarAListaCarrito(nuevoProducto, 'sumar');
-    observable.subscribe( listado => {
+    console.log('pressed');
+    const observable = this.cartService.addToCart(nuevoProducto, 'sumar');
+    observable.subscribe(listado => {
       this.productosEnCarrito = listado;
+      console.log('Agregar a carrito', this.productosEnCarrito);
     });
     return this.productosEnCarrito;
   }
