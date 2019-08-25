0a6447c4309b25c79e40f87603e503322470cfa2
Luis <ljmarquezg@gmail.com>
2019-08-25 11:52:23 +0200

    Update guard logged in user validation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,10 +11,10 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.getUser()) {
+    if (this.authService.isLoggedIn) {
       return true;
     }
-    this.router.navigate(['home']);
+    this.router.navigate(['/']);
     return false;
   }
 }
