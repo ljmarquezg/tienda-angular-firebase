f50aa2bfb79e02c7791781f992f86548fa42c57f
Luis <ljmarquezg@gmail.com>
2019-08-28 19:44:19 +0200

    New models

--- src/app/modelos/Items.ts
+++ src/app/modelos/Items.ts
@@ -2,18 +2,20 @@ import { Tipo } from './tipo';
 import { Producto } from './Producto';
 
 export class Item extends Producto {
-    id: number;
+    id: any;
     nombre: string;
     peso: number;
     tipo: Tipo;
     precio: number;
+    stock: number;
+    img: string;
     pesoK: number;
     cantidad: number;
     itemSubTotal: number;
     itemTotal: number;
 
     constructor(
-        id: number,
+        id: any,
         nombre: string,
         peso: number,
         tipo: Tipo,
@@ -21,9 +23,10 @@ export class Item extends Producto {
         stock: number,
         pesoK: number,
         cantidad: number,
+        img: string,
         itemSubTotal: number,
         itemTotal: number) {
-        super(id, nombre, peso, tipo, precio, stock);
+        super(id, nombre, peso, tipo, precio, stock, img);
         this.pesoK = pesoK;
         this.cantidad = cantidad;
         this.itemSubTotal = itemSubTotal;
--- src/app/modelos/Producto.ts
+++ src/app/modelos/Producto.ts
@@ -1,18 +1,20 @@
 import { Tipo } from './tipo';
 export class Producto {
-    id: number;
+    id: any;
     nombre: string;
     peso: number;
     precio: number;
     tipo: Tipo;
     stock: number;
+    img: string;
 
-    constructor(id: number, nombre: string, peso: number, tipo: Tipo, precio: number, stock: number) {
+    constructor(id: number, nombre: string, peso: number, tipo: Tipo, precio: number, stock: number, img: string) {
         this.id = id;
         this.nombre = nombre;
         this.peso = peso;
         this.tipo = tipo;
         this.precio = precio;
         this.stock = stock;
+        this.img = img;
     }
 }
