a552ea7b4980abbd278c6a40e97f68fd8de2025a
Luis <ljmarquezg@gmail.com>
2019-08-28 19:42:34 +0200

    Checkout component update

--- src/app/componentes/checkout/checkout.component.ts
+++ src/app/componentes/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart/cart.service';
+import { ProductoService } from 'src/app/services/producto/producto.service';
 import { Carrito } from 'src/app/modelos/Carrito';
 import { Item } from 'src/app/modelos/Items';
 import { Producto } from 'src/app/modelos/Producto';
@@ -11,37 +12,39 @@ import { Producto } from 'src/app/modelos/Producto';
 })
 export class CheckoutComponent implements OnInit {
 
+  carritoActual: Carrito;
   productosEnCarrito: Carrito;
-  listaProductos: Producto[] = [];
+  listaProductos: Item[] = [];
+  totalItems: number;
+  contador: number;
+  totalCarrito: number;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private productService: ProductoService) { }
 
   ngOnInit() {
-    const checkCarrito: Carrito = sessionStorage.cart;
-    if (checkCarrito && !this.productosEnCarrito) {
-      const listaProducto = this.cartService.obtenerProuctos();
-      listaProducto.subscribe(productos => {
-        this.listaProductos = productos;
-      });
-
-      console.log(this.listaProductos);
-      this.productosEnCarrito = this.cartService.checkSessionCart();
-      console.log(this.productosEnCarrito);
-      return;
-    }
-
-    const listado = this.cartService.getCartData();
-    listado.subscribe(listaProductos => {
-      this.productosEnCarrito = listaProductos;
-    });
+    this.getCart();
+    this.listaProductos = [];
+    this.totalItems = 0;
+    this.contador = 0;
+    this.totalCarrito = 0;
   }
 
   eliminarProductoDelCarrito(producto: Item): void {
-    console.log(producto);
     this.cartService.eliminarDelCarrito(producto);
   }
 
   modificarItemCarrito(item: Item, tipo: string) {
     this.cartService.modificarItemCarrito(item, tipo);
+  }
+
+  getCart(): Carrito {
+    this.cartService.getCartCollection();
+    this.cartService.getCartList().subscribe(carrito => {
+      this.carritoActual = carrito[0];
+      this.listaProductos = carrito[0].listaCarrito;
+      this.contador = carrito[0].contador;
+      this.totalItems = carrito[0].totalCarrito;
+    });
+    return this.carritoActual;
   }
 }
