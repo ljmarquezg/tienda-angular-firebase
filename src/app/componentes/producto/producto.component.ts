a599398c5457787a3df47c97e8194b0d8f19371d
Luis <ljmarquezg@gmail.com>
2019-08-28 19:45:07 +0200

    Modified Product component using angular material

--- src/app/componentes/producto/producto.component.ts
+++ src/app/componentes/producto/producto.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Producto } from 'src/app/modelos/Producto';
 import { ProductoService } from 'src/app/services/producto/producto.service';
 import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { Carrito } from 'src/app/modelos/Carrito';
 
 
 @Component({
@@ -11,28 +13,44 @@ import { Observable } from 'rxjs';
 })
 export class ProductoComponent implements OnInit {
 
-  @Output() enviarACarritoEmit = new EventEmitter<Producto>();
+  // @Output() enviarACarritoEmit = new EventEmitter<Producto>();
 
-  loading: boolean;
-  disableAddToCart: boolean = false;
-  listaProductos: Producto[] = [];
+  public loading: boolean;
+  public disableAddToCart: boolean = false;
+  public listaProductos: Producto[] = [];
+  public productosEnCarrito: Carrito;
+  // listaProductos: Item[] = [];
 
-  constructor(private productoService: ProductoService) {
+  constructor(private productoService: ProductoService, private cartService: CartService) {
   }
 
+  localProducts: Producto[] = [];
+
   ngOnInit() {
-    console.log('openingProducts');
     this.getProducts();
   }
 
-  getProducts(): Producto[]{
+  getProducts(): Producto[] {
     const listaProducto = this.productoService.obtenerProductos();
     listaProducto.subscribe(productos => {
       this.listaProductos = productos;
+      this.localProducts = productos;
     });
     return this.listaProductos;
   }
-  enviarAListaCarrito(producto: Producto): void {
-    this.enviarACarritoEmit.emit(producto);
+
+  enviarAListaCarrito(nuevoProducto: Producto): Carrito {
+    const observable = this.cartService.addToCart(nuevoProducto, 'sumar');
+    observable.subscribe(listado => {
+      this.productosEnCarrito = listado;
+    });
+    return this.productosEnCarrito;
   }
+
+  get listadoProductos(): Producto[] {
+    return this.listaProductos;
+  }
+  // enviarAListaCarrito(producto: Producto): void {
+  //   this.enviarACarritoEmit.emit(producto);
+  // }
 }
