38db64d5df5f463b56b56c25041df6410f1f0a7d
Luis <ljmarquezg@gmail.com>
2019-08-28 19:45:31 +0200

    modified product services

--- src/app/services/producto/producto.service.ts
+++ src/app/services/producto/producto.service.ts
@@ -3,19 +3,20 @@ import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/fire
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Producto } from 'src/app/modelos/Producto';
-import { Carrito } from 'src/app/modelos/Carrito';
-import { Item } from 'src/app/modelos/Items';
-import { Tipo } from 'src/app/modelos/tipo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-  private productosCollection: AngularFirestoreCollection<Producto>;
 
-  private productos: Observable<Producto[]>;
+  public productosCollection: AngularFirestoreCollection<Producto>;
+  public productos: Observable<Producto[]>;
 
   constructor(private db: AngularFirestore) {
+    this.getProductosCollection();
+  }
+
+  getProductosCollection() {
     this.productosCollection = this.db.collection<Producto>('producto');
     this.productos = this.productosCollection.snapshotChanges().pipe(
       map(product => {
@@ -33,4 +34,5 @@ export class ProductoService {
   obtenerProductos(): Observable<Producto[]> {
     return this.productos;
   }
+  
 }
